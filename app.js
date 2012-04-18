d786b5144213e0e8cf69df21479528d28b20899c
Jakob Jónasson <jakob.jonasson@gmail.com>
2012-04-18 23:30:22 +0000

    login

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ var express = require('express')
     ,routes = require('./routes')
     ,modules = require('./public/javascripts/socketServer');
 
+var MemoryStore = express.session.MemoryStore;
+
 var app = module.exports = express.createServer();
 var app = express.createServer();
 
@@ -12,9 +14,23 @@ app.configure(function(){
     app.set('view engine', 'jade');
     app.use(express.bodyParser());
     app.use(express.methodOverride());
-    app.use(app.router);
     app.use(express.static(__dirname + '/public'));
+    app.use(express.cookieParser());
+    app.use(express.session(
+    	{
+    		store: new MemoryStore(), secret:'kobbikobb'
+		}));
+	app.use(app.router);
 });
+
+// Helpers
+app.dynamicHelpers(
+  {
+    session: function(req, res) {
+      return req.session;
+    }
+  }
+);
 
 app.configure('development', function(){
     app.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
@@ -24,8 +40,26 @@ app.configure('production', function(){
     app.use(express.errorHandler());
 });
 
+
+function requiresLogin(req, res, next) {
+  if (req.session.user) {
+    next();
+  } else {
+    res.redirect('/login');
+  }
+}
+
+
 // Routes
-app.get('/', routes.index);
+app.get('/login', routes.login);
+app.get('/logout', routes.logout);
+
+app.get('/', requiresLogin, routes.index);
+
+app.post('/login', function(req, res) {
+      req.session.user = req.body.username;
+      res.redirect(req.body.redir || '/');
+});
 
 //Socket server
 modules.listen(app);
--- routes/index.js
+++ routes/index.js
@@ -1,3 +1,12 @@
 exports.index = function(req, res){
-  res.render('index')
+  res.render('index');
+};
+
+exports.login = function(req, res){
+  res.render('login', {redir: req.query.redir});
+};
+
+exports.logout = function(req, res){
+  delete req.session.user;
+  res.redirect('/login');
 };
